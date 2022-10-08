31f285c2041949c78bff5240bfef02aef387f57f
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-08 14:17:54 +0800

    [feat] add apy

---
+++ apy.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"math"
+
+	"github.com/0xVanfer/types"
+)
+
+func Apr2Apy[T types.Number](apr T) (apy float64) {
+	return math.Pow((1+types.ToFloat64(apr)/365), 365) - 1
+}
+
+func Apy2Apr[T types.Number](apy T) (apr float64) {
+	return (math.Pow(1+types.ToFloat64(apy), 1.0/365) - 1) * 365
+}
+
+func Apr2ApyWithCompoundTimes[T types.Number, S types.Number](apr T, compoundTimes S) (apy float64) {
+	return math.Pow((1+types.ToFloat64(apr)/types.ToFloat64(compoundTimes)), types.ToFloat64(compoundTimes)) - 1
+}
+
+func Apy2AprWithCompoundTimes[T types.Number, S types.Number](apy T, compoundTimes S) (apr float64) {
+	return (math.Pow(1+types.ToFloat64(apy), 1.0/types.ToFloat64(compoundTimes)) - 1) * types.ToFloat64(compoundTimes)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/0xVanfer/utils
 go 1.19
 
 require (
+	github.com/0xVanfer/types v0.1.2
 	github.com/ethereum/go-ethereum v1.10.25
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 )
@@ -10,5 +11,6 @@ require (
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
