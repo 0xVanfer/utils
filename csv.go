fabeab07697dda6247f364d234343b7084330c29
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-09 14:37:31 +0800

    [feat] edit csv

--- csv.go
+++ csv.go
@@ -4,9 +4,10 @@ import (
 	"encoding/csv"
 	"errors"
 	"os"
+	"strings"
 )
 
-// FilePath e.g. "./example.csv".
+// Read the csv file.
 func ReadCsv(filePath string) ([][]string, error) {
 	csvFile, err := os.Open(filePath)
 	if err != nil {
@@ -19,6 +20,7 @@ func ReadCsv(filePath string) ([][]string, error) {
 
 // If file exist, will cover the old file.
 func WriteNewCsv(filePath string, title []string, text [][]string) error {
+	// regular check
 	if len(text) == 0 {
 		return errors.New("text is empty")
 	}
@@ -65,6 +67,7 @@ func WriteToCsv(filePath string, title []string, text []string) error {
 	csvReader := csv.NewReader(csvFile)
 	csvWriter := csv.NewWriter(csvFile)
 
+	// file text already exist
 	fileText, err := csvReader.ReadAll()
 	if err != nil {
 		return err
@@ -82,8 +85,74 @@ func WriteToCsv(filePath string, title []string, text []string) error {
 			return err
 		}
 	} else {
+		// regular check
 		if len(fileText[0]) != len(title) {
 			return errors.New("title length not match")
+		}
+		// write text
+		err = csvWriter.Write(text)
+		if err != nil {
+			return err
+		}
+	}
+	// flush
+	csvWriter.Flush()
+	return csvWriter.Error()
+}
+
+// Write to csv line by line.
+//
+// Will not write if the value of "noRepeatTitle" already exists.
+func WriteToCsvResume(filePath string, title []string, text []string, noRepeatTitle string) error {
+	if len(text) != len(title) {
+		return errors.New("text length not match")
+	}
+	// read existing file, if not exist, create a new one
+	csvFile, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		// create new file
+		csvFile, err = os.Create(filePath)
+		if err != nil {
+			return err
+		}
+	}
+	defer csvFile.Close()
+	csvReader := csv.NewReader(csvFile)
+	csvWriter := csv.NewWriter(csvFile)
+
+	// file text already exist
+	fileText, err := csvReader.ReadAll()
+	if err != nil {
+		return err
+	}
+	// whether the file already has title
+	if len(fileText) == 0 {
+		// write title
+		err = csvWriter.Write(title)
+		if err != nil {
+			return err
+		}
+		// write text
+		err = csvWriter.Write(text)
+		if err != nil {
+			return err
+		}
+	} else {
+		// regular check
+		if len(fileText[0]) != len(title) {
+			return errors.New("title length not match")
+		}
+		var columeIndex int
+		for index, ti := range title {
+			if strings.EqualFold(ti, noRepeatTitle) {
+				columeIndex = index
+			}
+		}
+		// if repeat, return
+		for _, line := range fileText {
+			if strings.EqualFold(line[columeIndex], text[columeIndex]) {
+				return nil
+			}
 		}
 		// write text
 		err = csvWriter.Write(text)
