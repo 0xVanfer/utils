25ba7a68ebfc5c23dc8a5fcf69fbaf7ebd7975b1
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-13 18:59:02 +0800

    [feat] max and min support various amount of input

--- math.go
+++ math.go
@@ -2,16 +2,34 @@ package utils
 
 import "github.com/0xVanfer/types"
 
-func Min[T types.OrderedNumber](x, y T) T {
-	if x < y {
-		return x
+func Min[T types.OrderedNumber](numbers ...T) T {
+	if len(numbers) == 0 {
+		return 0
 	}
-	return y
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+	var min T = numbers[0]
+	for _, number := range numbers {
+		if number < min {
+			min = number
+		}
+	}
+	return min
 }
 
-func Max[T types.OrderedNumber](x, y T) T {
-	if x > y {
-		return x
+func Max[T types.OrderedNumber](numbers ...T) T {
+	if len(numbers) == 0 {
+		return 0
 	}
-	return y
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+	var max T = numbers[0]
+	for _, number := range numbers {
+		if number > max {
+			max = number
+		}
+	}
+	return max
 }
