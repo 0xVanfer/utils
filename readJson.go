7ee34fdb7f0afdfccd0e6ab76c80bbacc0fa5009
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-12-01 13:11:31 +0800

    [feat] read json add more funcs

--- readJson.go
+++ readJson.go
@@ -6,6 +6,29 @@ import (
 	"os"
 )
 
+// Read json file into []byte.
+func ReadJsonBytes(path string) ([]byte, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+	byteValue, err := io.ReadAll(jsonFile)
+	return byteValue, err
+}
+
+// Read json file into []byte and convert into string.
+func ReadJsonString(path string) (string, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer jsonFile.Close()
+	byteValue, err := io.ReadAll(jsonFile)
+	return string(byteValue), err
+}
+
+// Read json file into []byte and unmarshal into []string.
 func ReadJsonSlice(path string) ([]string, error) {
 	jsonFile, err := os.Open(path)
 	if err != nil {
@@ -19,4 +42,14 @@ func ReadJsonSlice(path string) ([]string, error) {
 	var slice []string
 	err = json.Unmarshal([]byte(byteValue), &slice)
 	return slice, err
+}
+
+// Read json file into []byte and unmarshal into v.
+func ReadJson(path string, v any) error {
+	bytes, err := ReadJsonBytes(path)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bytes, v)
+	return err
 }
