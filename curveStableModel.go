dc5e83f9b990f368032267f25de01a006c4297cb
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2023-01-05 16:49:23 +0800

    [feat] add curve all kinds of stable model

--- curveStableModel.go
+++ curveStableModel.go
@@ -1,119 +1,215 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"math/big"
 
 	"github.com/0xVanfer/types"
 )
 
-// Curve stable token swap model. Two tokens vesion.
+// Curve stable coin model.
 //
 // Example:
 //
-//	info := &utils.CurveStableModel{
-//		A:        1000,
-//		BalanceX: big.NewInt(1).Mul(big.NewInt(5000000), big.NewInt(1e18)),
-//		BalanceY: big.NewInt(1).Mul(big.NewInt(5000000), big.NewInt(1e18)),
-//		Dx:       big.NewInt(1).Mul(big.NewInt(-1000), big.NewInt(1e18)),
+//	info := &CurveStableCoinModel{
+//		A:     50,
+//		N:     2,
+//		X_Num: 0,
+//		Y_Num: 1,
+//		Fee:   0.0004,
+//		Balances: []*big.Int{
+//			big.NewInt(1).Mul(big.NewInt(409880), big.NewInt(1e18)),
+//			big.NewInt(1).Mul(big.NewInt(244647), big.NewInt(1e18)),
+//		},
+//		Dx: big.NewInt(1).Mul(big.NewInt(1), big.NewInt(1e18)),
 //	}
 //	info.CalcDy()
 //	utils.PrettyJsonPrintln(info)
-type CurveStableModel struct {
+type CurveStableCoinModel struct {
 	// Input info.
-	A int64 // Amplification.
+	A   int64   // Amplification. A != 0.
+	N   int64   // Token number. N >= 2.
+	Fee float64 // Curve Fee. Usually 0.0004.
 
 	// Input info.
 	// Token amount info. Must be multiplied by 1e18.
-	BalanceX *big.Int // Pool balance of token x.
-	BalanceY *big.Int // Pool balance of token y.
-	Dx       *big.Int // Swap amount of token x.
+	Balances []*big.Int // Pool balance of tokens. len(Balances) == N.
+	Dx       *big.Int   // Swap amount of token x.
+	X_Num    int64      // The serial number of the token to be swaped. Related to `Balances`.
+	Y_Num    int64      // The serial number of the token to be swaped to. Related to `Balances`.
 
 	// Output info.
-	D          *big.Int // Variable D of the pool.
-	ExpectedDy *big.Int // Expected give out amount of token y.
-	Slippage   float64  // The slippage
+	sum  *big.Int // Sum of all balances.
+	prod *big.Int // Production of all balances.
+	an   *big.Int // A * n
+	ann  *big.Int // A * n**n
+	D    *big.Int // Variable D of the pool.
+
+	// Output info.
+	ExpectedFee *big.Int // Expected fee to be charged.
+	ExpectedDy  *big.Int // Expected give out amount of token y.
 }
 
-// Calculate D of the pool, expected dy and slippage of the swap.
-// A, BalanceX, BalanceY must not be nil.
-func (details *CurveStableModel) CalcDy() {
+// Regulatory check of details.
+//
+// Requirements:
+//
+//	A != 0;
+//	len(Balances) == N >= 2;
+//	Any of Balances !=0;
+//	X_Num != Y_Num;
+//	0 <= X_Num, Y_Num < N;
+func (details *CurveStableCoinModel) regulatoryCheck() (err error) {
 	if details.A == 0 {
-		fmt.Println("A cannot be 0")
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: A != 0.")
 		return
 	}
-	if details.BalanceX == nil {
-		fmt.Println("BalanceX cannot be nil")
+	if details.N < 2 {
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: N >= 2.")
 		return
 	}
-	if details.BalanceY == nil {
-		fmt.Println("BalanceY cannot be nil")
+	if int64(len(details.Balances)) != details.N {
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: len(Balances) == N.")
 		return
 	}
-	x1 := details.BalanceX
-	x2 := details.BalanceY
-	// balance1 + balance2; x1 add x2
-	x1ax2 := big.NewInt(1).Add(x1, x2)
-	// balance1 * balance2; x1 mul x2
-	x1mx2 := big.NewInt(1).Mul(x1, x2)
-
-	// https://docs.balancer.fi/concepts/math/stable-math
-	// To get D, solve the function:
-	// D^3 + (16 * A * X1 * X2 - 4 * X1 * X2) * D - 16 * A * X1 * X2 * (X1 + X2) = 0
-	// D^3 + p * D + q = 0
-	// p = 16 * A * X1 * X2 - 4 * X1 * X2
-	p := big.NewInt(1).Sub(big.NewInt(1).Mul(big.NewInt(16*details.A), x1mx2), big.NewInt(1).Mul(big.NewInt(4), x1mx2))
-	// q = - 16 * A * X1 * X2 * (X1 + X2)
-	q := big.NewInt(1).Mul(big.NewInt(-16*details.A), big.NewInt(1).Mul(x1ax2, x1mx2))
-
-	// p/3
-	pd3 := big.NewInt(1).Div(p, big.NewInt(3))
-	// q/2
-	qd2 := big.NewInt(1).Div(q, big.NewInt(2))
-
-	// (q/2)^2
-	i2q := big.NewInt(1).Exp(qd2, big.NewInt(2), nil)
-	// (p/3)^3
-	i3p := big.NewInt(1).Exp(pd3, big.NewInt(3), nil)
-	// (i2q + i3p)^1/2
-	internal := BigSqrt(big.NewInt(1).Add(i2q, i3p), 2)
-
-	// (-q/2 + internal)^1/3
-	re1 := BigSqrt(big.NewInt(1).Sub(internal, qd2), 3)
-	// (-q/2 - internal)^1/3 = - (q/2 + internal)^1/3
-	re2 := big.NewInt(1).Mul(BigSqrt(big.NewInt(1).Add(internal, qd2), 3), big.NewInt(-1))
-
-	// D = re1 + re2
-	D := big.NewInt(1).Add(re1, re2)
-	details.D = D
-
-	// Do not calculate the dy.
-	dx1 := details.Dx
-	if (dx1 == nil) || (dx1.Cmp(big.NewInt(0)) == 0) {
+	for _, balance := range details.Balances {
+		if balance.Cmp(big.NewInt(0)) == 0 {
+			//lint:ignore ST1005 sentence error needed
+			err = errors.New("Require: No 0 in `Balances`.")
+			return
+		}
+	}
+	if details.X_Num == details.Y_Num {
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: X_Num != Y_Num.")
 		return
 	}
-	// x1 + dx1
-	x1adx1 := big.NewInt(1).Add(x1, dx1)
+	if details.X_Num < 0 || details.X_Num >= details.N {
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: 0 <= X_Num < N.")
+		return
+	}
+	if details.Y_Num < 0 || details.Y_Num >= details.N {
+		//lint:ignore ST1005 sentence error needed
+		err = errors.New("Require: 0 <= Y_Num < N.")
+		return
+	}
+	return
+}
 
-	// Trading.
-	// https://dev.balancer.fi/resources/pool-math/stable-math
-	// Swap token1 for token2.
-	// y^2 + (D/4A + (X1 + dX1) - D) * y - D^3 / (16A * (X1 + dX1)) = 0
-	// y^2 + b * x + c = 0
-	// b = D/4A + (X1 + dX1) - D
-	b := big.NewInt(1).Sub(big.NewInt(1).Add(big.NewInt(1).Div(D, big.NewInt(4*details.A)), x1adx1), D)
-	// c = -D^3 / (16A * (X1 + dX1))
-	c := big.NewInt(1).Mul(big.NewInt(1).Div(big.NewInt(1).Exp(D, big.NewInt(3), nil), big.NewInt(1).Mul(big.NewInt(16*details.A), x1adx1)), big.NewInt(-1))
+// Calculate sum, prod, an, ann.
+func (details *CurveStableCoinModel) calcSP() {
+	details.an = big.NewInt(details.A * details.N)
+	details.ann = big.NewInt(1).Mul(big.NewInt(1).Exp(big.NewInt(details.N), big.NewInt(details.N), nil), big.NewInt(details.A))
+	// fmt.Println(details.ann)
 
-	// (b^2 - 4 * a * c)^1/2; a = 1
-	delta := BigSqrt(big.NewInt(1).Sub(big.NewInt(1).Exp(b, big.NewInt(2), nil), big.NewInt(1).Mul(big.NewInt(4), c)), 2)
-	// y = (-b + delta) / 2
-	y := big.NewInt(1).Div(big.NewInt(1).Sub(delta, b), big.NewInt(2))
-	// dx2 = x2 - y
-	dx2 := big.NewInt(1).Sub(x2, y)
-	details.ExpectedDy = dx2
-	// Price difference of dx1 and dx2.
-	diff := big.NewInt(1).Abs(big.NewInt(1).Sub(dx1, dx2))
-	slippage := types.ToFloat64(diff) / types.ToFloat64(big.NewInt(1).Abs(details.Dx))
-	details.Slippage = slippage
+	newSum := big.NewInt(0)
+	newProd := big.NewInt(1)
+	for _, balance := range details.Balances {
+		// sum += balance
+		newSum = big.NewInt(1).Add(newSum, balance)
+		// prod += balance
+		newProd = big.NewInt(1).Mul(newProd, balance)
+	}
+	details.sum = newSum
+	details.prod = newProd
+
+}
+
+// Must be the first to be called.
+func (details *CurveStableCoinModel) calcD() {
+	details.calcSP()
+	// D of the previous round.
+	DPrev := big.NewInt(0)
+	_ = DPrev
+	// D is initialized as details.sum
+	D := details.sum
+
+	for i := 0; i < 255; i++ {
+		D_P := D
+		for _, balance := range details.Balances {
+			// dp = dp * D / (balance * N)
+			D_P = big.NewInt(1).Div(big.NewInt(1).Mul(D_P, D), big.NewInt(1).Mul(balance, big.NewInt(details.N)))
+		}
+		DPrev = D
+		// (An * sum + D_P * N) * D
+		up_ := big.NewInt(1).Mul(D, big.NewInt(1).Add(big.NewInt(1).Mul(details.an, details.sum), big.NewInt(1).Mul(D_P, big.NewInt(details.N))))
+		// (An - 1) * D + (N + 1) * D_P
+		down_ := big.NewInt(1).Add(big.NewInt(1).Mul(big.NewInt(1).Sub(details.an, big.NewInt(1)), D), big.NewInt(1).Mul(big.NewInt(details.N+1), D_P))
+		D = big.NewInt(1).Div(up_, down_)
+		// Abs of D - DPrev.
+		diff := big.NewInt(1).Abs(big.NewInt(1).Sub(D, DPrev))
+		// diff <= 1 return D
+		if diff.Cmp(big.NewInt(1)) != 1 {
+			details.D = D
+			return
+		}
+	}
+}
+
+// Calculate everything.
+func (details *CurveStableCoinModel) CalcDy() {
+	err := details.regulatoryCheck()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Calculate the previous D.
+	details.calcD()
+
+	var an *big.Int = big.NewInt(details.A * details.N)
+	var sum *big.Int = big.NewInt(0)
+	var prod *big.Int = big.NewInt(1)
+	var D_P *big.Int = details.D
+	var yStart *big.Int
+
+	for i := 0; i < int(details.N); i++ {
+		var balance *big.Int
+		if i == int(details.X_Num) {
+			// If is the token to be swaped, continue, balance += dx.
+			balance = big.NewInt(1).Add(details.Balances[i], details.Dx)
+		} else if i == int(details.Y_Num) {
+			yStart = details.Balances[i]
+			// If is the token to be swaped to, continue.
+			// Neither sum nor product will use this.
+			continue
+		} else {
+			balance = details.Balances[i]
+		}
+		sum = big.NewInt(1).Add(sum, balance)
+		prod = big.NewInt(1).Mul(prod, balance)
+		D_P = big.NewInt(1).Div(big.NewInt(1).Mul(D_P, details.D), big.NewInt(1).Mul(balance, big.NewInt(details.N)))
+	}
+
+	c := big.NewInt(1).Div(big.NewInt(1).Mul(D_P, details.D), big.NewInt(1).Mul(an, big.NewInt(details.N)))
+	b := big.NewInt(1).Add(sum, big.NewInt(1).Div(details.D, an))
+	y := details.D
+	yPrev := big.NewInt(0)
+	_ = yPrev
+
+	for i := 0; i < 255; i++ {
+		yPrev = y
+		// y * y +c
+		up_ := big.NewInt(1).Add(big.NewInt(1).Mul(y, y), c)
+		// 2 * y + b - D
+		down_ := big.NewInt(1).Sub(big.NewInt(1).Add(big.NewInt(1).Mul(big.NewInt(2), y), b), details.D)
+		y = big.NewInt(1).Div(up_, down_)
+
+		// Abs of y - yPrev.
+		diff := big.NewInt(1).Abs(big.NewInt(1).Sub(y, yPrev))
+		// diff <= 1, y is decided
+		if diff.Cmp(big.NewInt(1)) != 1 {
+			dy := big.NewInt(1).Sub(yStart, y)
+			details.ExpectedFee = big.NewInt(1).Div(big.NewInt(1).Mul(dy, big.NewInt(types.ToInt64(details.Fee*math.Pow10(18)))), big.NewInt(int64(math.Pow10(18))))
+			details.ExpectedDy = big.NewInt(1).Sub(dy, details.ExpectedFee)
+			// fmt.Println(details.ExpectedFee)
+			// fmt.Println(details.ExpectedDy)
+			return
+		}
+	}
 }
--- curveStableModel_test.go
+++ curveStableModel_test.go
@@ -5,13 +5,26 @@ import (
 	"testing"
 )
 
-func TestCurveStableModel(t *testing.T) {
-	info := &CurveStableModel{
-		A:        1000,
-		BalanceX: big.NewInt(1).Mul(big.NewInt(5000000), big.NewInt(1e18)),
-		BalanceY: big.NewInt(1).Mul(big.NewInt(5000000), big.NewInt(1e18)),
-		Dx:       big.NewInt(1).Mul(big.NewInt(-1000), big.NewInt(1e18)),
+func TestCurveStableCoinModel(t *testing.T) {
+	big5k := big.NewInt(1).Mul(big.NewInt(5000), big.NewInt(1e18))
+	_ = big5k
+	big5m := big.NewInt(1).Mul(big.NewInt(5000000), big.NewInt(1e18))
+	_ = big5m
+
+	info := &CurveStableCoinModel{
+		A:     50,
+		N:     2,
+		X_Num: 0,
+		Y_Num: 1,
+		Fee:   0.0004,
+		Balances: []*big.Int{
+			big.NewInt(1).Mul(big.NewInt(409880), big.NewInt(1e18)),
+			big.NewInt(1).Mul(big.NewInt(244647), big.NewInt(1e18)),
+		},
+		Dx: big.NewInt(1).Mul(big.NewInt(1), big.NewInt(1e18)),
 	}
 	info.CalcDy()
 	PrettyJsonPrintln(info)
+	// fmt.Println(info.sum, info.ann, info.prod)
+
 }
